cf9384e7972403c6bbf186cd64ad58ce3caedc2b
ArixElo <patroxgamer@gmail.com>
2025-12-28 12:59:25 +0100

    repo: install-config: rewrite it completely
    It has now an ASCII art, and also it checks whether you have installed required packages or not
    
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- install-config.sh
+++ install-config.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
-echo "Checking if you have installed required packages..."
-sudo yay -Qi cava wttrbar kitty
-echo "installing required packages..."
-sudo omarchy-install-terminal kitty
-sudo yay -S cava wttrbar
-echo "Copying all configs to ~/.config..."
-cp -r *.* ~/.config
-echo "Restarting waybar..."
-sudo pkill waybar && hyprctl dispatch exec waybar
-echo "Applying config is done, but for better experience: Change your location for wttrbar, and reboot your machine."
+
+function __install() {
+    clear
+
+   
+cat <<\EOF
+    ___         _      ________     _          ____                            __             ______            _____      
+   /   |  _____(_)  __/ ____/ /___ ( )_____   / __ \____ ___  ____ ___________/ /_  __  __   / ____/___  ____  / __(_)___ _
+  / /| | / ___/ / |/_/ __/ / / __ \|// ___/  / / / / __ `__ \/ __ `/ ___/ ___/ __ \/ / / /  / /   / __ \/ __ \/ /_/ / __ `/
+ / ___ |/ /  / />  </ /___/ / /_/ / (__  )  / /_/ / / / / / / /_/ / /  / /__/ / / / /_/ /  / /___/ /_/ / / / / __/ / /_/ / 
+/_/  |_/_/  /_/_/|_/_____/_/\____/ /____/   \____/_/ /_/ /_/\__,_/_/   \___/_/ /_/\__, /   \____/\____/_/ /_/_/ /_/\__, /  
+                                                                                 /____/                           /____/ 
+EOF
+    printf "%s\n\n" "(c) Copyright, ArixElo ~ 2025."
+
+    echo "Checking if you have installed required packages..."
+    
+    if yay -Qi cava &>/dev/null && yay -Qi kitty &>/dev/null && yay -Qi wttrbar &>/dev/null; then
+        echo "It looks like you have already installed all packages"
+        __copy
+    else
+        echo "Installing required packages..."
+        sudo omarchy-install-terminal kitty && yay -S cava wttrbar
+        __copy
+    fi
+}
+
+function __copy() {
+    echo "Copying all configs to ~/.config..."
+    cp -r */ ~/.config/
+    echo "Restarting waybar..."
+    pkill waybar && hyprctl dispatch exec waybar
+    echo "Applying config is done, but for better experience: Change your location for wttrbar, and reboot your machine."
+}
+
+__install
