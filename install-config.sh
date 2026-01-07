06d0209fc386def7a18a2a0ca24c8a6222a98e45
ArixElo <patroxgamer@gmail.com>
2026-01-07 04:09:35 +0100

    repo: install-config: Add zsh with syntax highlighting, autocompletion and starship
    also apply zsh as default shell and copy it's config where should be
    
    Signed-off-by: ArixElo <patroxgamer@gmail.com>

--- install-config.sh
+++ install-config.sh
@@ -16,21 +16,24 @@ EOF
 
     echo "Checking if you have installed required packages..."
     
-    if yay -Qi cava &>/dev/null && yay -Qi kitty &>/dev/null && yay -Qi wttrbar &>/dev/null; then
+    if yay -Qi cava &>/dev/null && yay -Qi kitty &>/dev/null && yay -Qi wttrbar &>/dev/null && yay -Qi starship &>/dev/null && yay -Qi zsh &>/dev/null && yay -Qi zsh-autosuggestions &>/dev/null && yay -Qi zsh-syntax-highlighting &>/dev/null; then
         echo "It looks like you have already installed all packages"
         __copy
     else
         echo "Installing required packages..."
-        yay -S cava wttrbar kitty
+        yay -S cava wttrbar kitty starship zsh zsh-autosuggestions zsh-syntax-highlighting
         __copy
     fi
 }
 
 function __copy() {
-    echo "Copying all configs to ~/.config and ~/.local also changing your terminal..."
+    echo "Copying all configs to ~/.config and ~/.local also changing your terminal and your shell..."
     cp -r */ ~/.config/
     cp tiling-v2.conf ~/.local/share/omarchy/default/hypr/bindings
     cat "kitty.desktop" > ~/.config/xdg-terminals.list
+    chsh $USER /usr/bin/zsh && sudo chsh /usr/bin/zsh
+    cp .zshrc ~/home/$USER/
+    source ~/.zshrc
     echo "Restarting waybar..."
     pkill waybar && hyprctl dispatch exec waybar
     echo "Applying config is done, but for better experience: Change your location for wttrbar, and reboot your machine."
